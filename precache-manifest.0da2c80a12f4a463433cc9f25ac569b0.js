cae576bd75be951a9ffbc9722b3d6a5eb350c694
Your Name <talhadar>
2020-08-05 21:13:38 +0200

    Updates

---
+++ precache-manifest.0da2c80a12f4a463433cc9f25ac569b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5aad44d7c1da226653d79e8a46e243",
+    "url": "/Agency/index.html"
+  },
+  {
+    "revision": "19742fafbf1cda4cf921",
+    "url": "/Agency/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5eb37915f6e384a296f",
+    "url": "/Agency/static/js/2.215f1fcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Agency/static/js/2.215f1fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19742fafbf1cda4cf921",
+    "url": "/Agency/static/js/main.717c21a0.chunk.js"
+  },
+  {
+    "revision": "f54fb67bfaa7396c68a6",
+    "url": "/Agency/static/js/runtime-main.586c99bd.js"
+  }
+]);
